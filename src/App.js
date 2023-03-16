d17ab20af244912d104cf8c127d0ab7a14bed4b6
Shanice Valentine <shanmv13@gmail.com>
2023-03-15 22:10:09 -0400

    updated js and css files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchEngine from "./SearchEngine";
+
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App Wrapper">
+      <Container sm className="container container-main">
+        <SearchEngine />
+      </Container>
     </div>
   );
 }
